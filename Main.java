fa362f715a0ddb2e92594d05d7ba0da8fddd987a
Angelina Namwandi <115535250+AngelinaNamwandi@users.noreply.github.com>
2024-04-29 22:15:10 +0200

    Add files via upload
    Main Class

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create server node
+        ServerNode server = new ServerNode();
+
+        // Create client nodes
+        ClientNode client1 = new ClientNode("Angie Eazy");
+        ClientNode client2 = new ClientNode("Ruvarashe");
+        ClientNode client3 = new ClientNode("Thandeka");
+
+        // Add client nodes to the server
+        server.addClient(client1);
+        server.addClient(client2);
+        server.addClient(client3);
+
+        // Send messages from clients
+        client1.send("Hello", server);
+        client2.send("Ndeip", server);
+        client3.send("Unjani", server);
+    }
+}
